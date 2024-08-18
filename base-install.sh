7dc9f1674cee6091623aeda8866ed34307d2da95
fug4r <166860013+fug4r@users.noreply.github.com>
2024-08-18 00:06:48 +0000

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Taken from https://gitlab.com/eflinux/arch-basic/-/blob/master/base-uefi.sh?ref_type=heads, to modify
-
-echo -n "Enter your localtime region" 
-read region
-
 ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
 hwclock --systohc
 sed -i '178s/.//' /etc/locale.gen
@@ -17,28 +12,16 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
 
-# You can add xorg to the installation packages, I usually add it at the DE or WM install script
-# You can remove the tlp package if you are installing on a desktop or vm
-
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
-
-# pacman -S --noconfirm xf86-video-amdgpu
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
-
-grub-mkconfig -o /boot/grub/grub.cfg
-
-systemctl enable NetworkManager
-systemctl enable bluetooth
+systemctl enable NetworkManager.service
+systemctl enable bluetooth.service
 systemctl enable cups.service
 systemctl enable ipp-usb.service
-systemctl enable sshd
-systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable sshd.service
+systemctl enable avahi-daemon.service
+systemctl enable firewalld.service
+systemctl enable acpid.service
+systemctl enable tlp.service
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable logrotate.timer
-systemctl enable libvirtd
-systemctl enable firewalld
-systemctl enable acpid
+systemctl enable updatedb.timer
