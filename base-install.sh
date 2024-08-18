bbfd35d70c8646229f5d2cc8c91a14aaa225d08a
fug4r <fug4r@tutanota.com>
2024-08-18 20:38:16 +0200

    Shell script

--- base-install.sh
+++ base-install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
-hwclock --systohc
-sed -i '178s/.//' /etc/locale.gen
-locale-gen
+# General settings
+
+read -p "Enter hostname: " hostname
+echo "Config..."
+
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "KEYMAP=de_CH-latin1" >> /etc/vconsole.conf
-echo "arch" >> /etc/hostname
+echo "$hostname" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
+
+
+echo "Root passwd"
 echo root:password | chpasswd
+echo "Root passwd set"
 
 systemctl enable NetworkManager.service
 systemctl enable bluetooth.service
