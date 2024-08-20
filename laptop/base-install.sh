3aafc7906d446fdc8f5f3eaf12976cd3faa2a327
fug4r <fug4r@tutanota.com>
2024-08-20 20:05:25 +0000

    Updated script

--- laptop/base-install.sh
+++ laptop/base-install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Installing packages
+echo "Installing packages..."
 pacman -S --needed $(grep -v '^#' packages.txt)
 
 
 # General settings
-echo "Configuring localtime..."
+echo -e "\nConfiguring localtime..."
 read -p "Enter timezone region: " region
 read -p "Enter timezone city: " city
 ln -sf /usr/share/zoneinfo/$region/$city /etc/localtime
 hwclock --systohc
+echo "Localtime configured."
 
 echo -e "\nConfiguring ntp..."
 cp ./timesyncd.conf /etc/systemd/timesyncd.conf
+echo "Ntp configured."
 
 echo -e "\nConfiguring locale and hostname..."
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
@@ -23,45 +26,54 @@ read -p "Enter hostname: " hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
-echo ""
+echo "Locale and hostname configured."
 
 
-# Ramdisk & custom edid binary
+# Initramfs & custom edid binary
+echo -e "\nConfiguring and generating initramfs..."
 mkdir -p /usr/lib/firmware/edid/ && cp ./edid.bin /usr/lib/firmware/edid/
 mkinitcpio -p linux
+echo "Initramfs set up."
 
 
 # Installing grub
+echo -e "\nInstalling grub..."
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 
-echo -e "\nAssuming cryptdevice is /dev/mapper/arch-brfs and getting its UUID"
+echo -e "\nSetting up grub config file..."
+echo "Assuming cryptdevice is /dev/mapper/arch-brfs and getting its UUID"
 cryptuuid=`blkid | grep arch-btrfs | cut -d'"' -f 2` 
 echo "UUID got: $cryptuuid"
 
-echo "Incorporating UUID in grub config file"
+echo "Incorporating UUID in grub config file..."
 # Copying from default config to prevent its modification
 cp ./grub.default ./grub
 sed -i -e "s/<>/$cryptuuid/g" grub
+echo "Grub settings finished."
 
-echo -e "\nCopying final grub file to /etc/default/grub and making grub config"
+echo -e "\nMoving final grub file to /etc/default/grub and making grub config..."
 mv ./grub /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
+echo "Grub setup finished."
 
 
 # Adding admin user and setting root password
-echo -e "\nCreating default admin user..."
+echo -e "\nCreating admin user..."
 read -p "Enter username: " username
 
 useradd -mG wheel $username
-echo "\nChanging $username password..."
-echo $username:password | chpassword
+echo -e "\nChanging $username password..."
+passwd $username
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
+echo "Created user $username and configured sudoers file."
 
 echo -e "\nChanging root password..."
-echo root:password | chpasswd
+passwd root
+echo "Root password changed."
 
 
 # Enabling systemd services
+echo -e "\nEnabling various systemd services..."
 systemctl enable NetworkManager.service
 systemctl enable bluetooth.service
 systemctl enable cups.service
@@ -76,6 +88,7 @@ systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable logrotate.timer
 systemctl enable updatedb.timer
+echo "Relevant systemd services enabled."
 
 
 # Fishing message
